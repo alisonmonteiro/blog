5f81d277448b1fe53c61d904a2f577ce32ded83b
Alison Monteiro <alisonmonteiro.10@gmail.com>
2019-08-10 12:05:24 -0300

    Initial commit

---
+++ blog.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMeta: {
+    title: 'Alison Monteiro',
+    author: 'Alison Monteiro',
+    image: '/static/site-feature.png',
+    description: 'Alison Monteiro Website',
+    siteUrl: 'https://alisonmonteiro.now.sh',
+    social: {
+      twitter: 'alisonmonteir1'
+    },
+    postsPerPage: 20
+  }
+}
