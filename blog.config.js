bfe6b355383c936cc20fc14a86d4ac763e810f65
Alison Monteiro <alisonmonteiro.10@gmail.com>
2021-07-08 21:21:42 -0300

    Update bio

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'Alison Monteiro',
     author: 'Alison Monteiro',
     image: '/static/site-feature.png',
-    description: 'Alison Monteiro Website',
+    description: 'Alison Monteiro\'s website. A 25yo self-motivated Software Developer based in Fortaleza, Brazil. Who likes to create things for the web.',
     siteUrl: 'https://alisonmonteiro.com.br',
     social: {
       twitter: 'alisonmonteir1'
